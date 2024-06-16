1f01e677b793b62fbc77db07a1e77d32aed640d5
dwkcoelho <dwkcoelho@hotmail.com>
2024-06-16 18:51:48 +1000

    First contact with React and JSX

---
+++ One Bit Code/Full Stack Javascript/React/Exercise 1/src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <>
+    <div className="app">
+      <img src="/react.png" alt="React logo" />
+      <h1>React</h1>
+      <p>The user interface and natives library</p>
+      <div>
+        <button>Learn React</button>
+        <button>API reference</button>
+      </div>
+      <hr />
+      <h2>Create component user interface</h2>
+      <p>React allows you to build user interfaces from individual pieces called components.</p>
+      <hr />
+      <h2>Write components with code and markup</h2>
+      <p>React components are JavaScript functions. The markup syntax is called JSX. It is an extension of the JavaScript syntax popularized by React.</p>
+      <hr />
+      <h2>Next steps</h2>
+      <ul>
+        <li>Using dynamic data in JSX</li>
+        <li>Creation of new components</li>
+        <li>Component styling</li>
+        <li>Component reuse</li>
+        <li>Use of props and children</li>
+        <li>Using JavaScript events</li>
+      </ul>
+    </div>  
+    </>
+  )
+}
+
+export default App
