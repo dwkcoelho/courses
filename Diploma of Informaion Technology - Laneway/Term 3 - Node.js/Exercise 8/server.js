0ae2d9342a3f131f9b7c192b66f40e32e43ef85e
dwkcoelho <dwkcoelho@hotmail.com>
2024-07-29 17:56:09 +1000

    MailChimp Fixed!

--- Diploma of Informaion Technology - Laneway/Term 3 - Node.js/Exercise 8/server.js
+++ Diploma of Informaion Technology - Laneway/Term 3 - Node.js/Exercise 8/server.js
@@ -1,42 +1,42 @@
 const https = require('https');
 const axios = require('axios');
-const path = require('node:path')
+const path = require('node:path'); 
 const express = require('express');
 const app = express();
 const { key } = require('./mailChimpKey'); 
 
-
-// app.use(express.static(path.join(__dirname)))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.post('/', async (req, res) => {
     const { firstName, lastName, email } = req.body;
 
-    try{
+    try {
         const response = await axios.post(
-            "htpps://us14.api.mailchimp.com/3.0/lists/bf260a56fa/members",
+            "https://us14.api.mailchimp.com/3.0/lists/bf260a56fa/members",
             {
                 email_address: email,
                 status: "pending",
                 merge_fields: {
-                  FNAME: firstName,
-                  LNAME: lastName,
+                    FNAME: firstName,
+                    LNAME: lastName,
                 },
             },
             {
-                headres: {
+                headers: {
                     Authorization: `Bearer ${key}`,
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
-            })
-        } catch(error) {
-            console.log(error)
-        }
-    });
+            }
+        );
+        res.status(200).send("Subscription successful!");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Subscription failed.");
+    }
+});
 
-// Start the Express server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
