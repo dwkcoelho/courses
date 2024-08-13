c40f49ad52fe112a31420e057f780ac4e28cf737
dwkcoelho <dwkcoelho@hotmail.com>
2024-08-13 18:56:06 +1000

    CORS

---
+++ Diploma of Informaion Technology - Laneway/Term 3 - Node.js/Exercise 12/Front-End/public/js/key.js
@@ -0,0 +1,3 @@
+const key = 'OCHGu2ZbLvjozjbbf3MFRQ==TNLRkQRj6C7G2P5D'
+
+module.exports = { key }
---
+++ Diploma of Informaion Technology - Laneway/Term 3 - Node.js/Exercise 12/Front-End/server.js
@@ -0,0 +1,13 @@
+const path = require("node:path")
+const express = require("express");
+const app = express();
+const port = 3001;
+app.use(express.static(path.join(__dirname)))
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, function () {
+  console.log(`Server Running on port ${port}`);
+});
